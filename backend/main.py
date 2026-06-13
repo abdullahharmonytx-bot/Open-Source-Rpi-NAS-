2c97f43c9f6857c9ec755e78ace8ec4bcabedce4
TheC03L <abdullah.harmonytx@gmail.com>
2026-06-13 14:48:58 -0500

    Add FastAPI welcome and setup routes

--- backend/main.py
+++ backend/main.py
@@ -1,1 +1,43 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
+app = FastAPI(title="ALPHA")
+
+@app.get("/", response_class=HTMLResponse)
+async def welcome():
+    return """
+    <html>
+        <head>
+            <title>ALPHA</title>
+        </head>
+        <body>
+            <h1>Welcome to ALPHA</h1>
+            <p>Open Source Raspberry Pi NAS</p>
+
+            <a href="/setup">
+                <button>Start Setup</button>
+            </a>
+        </body>
+    </html>
+    """
+
+@app.get("/setup", response_class=HTMLResponse)
+async def setup():
+    return """
+    <html>
+        <head>
+            <title>ALPHA Setup</title>
+        </head>
+        <body>
+            <h1>Create Administrator</h1>
+
+            <form>
+                <input placeholder="Name"><br><br>
+                <input placeholder="Username"><br><br>
+                <input type="password" placeholder="Password"><br><br>
+
+                <button>Create Admin</button>
+            </form>
+        </body>
+    </html>
+    """
